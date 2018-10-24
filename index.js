46cac57906180cfa0f9d47f6354eb7557c795481
Crystal B <crystalbohacek@gmail.com>
2018-10-24 18:09:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,5 +72,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (arguments.length === 0 || arguments === undefined){
     return "Sorry, we don't have a credit card on file for you."
+  } else {
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
