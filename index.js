67199fe72b16bf334e23566fc89d89c4db28662a
Crystal B <crystalbohacek@gmail.com>
2018-10-24 18:16:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   var str = "";
-  if (arguments.length === 0 || arguments === undefined){
+  if (arguments.length === 0 || cardNumber === undefined || isNaN(cardNumber)){
     return "Sorry, we don't have a credit card on file for you."
   } else {
     str+= `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
