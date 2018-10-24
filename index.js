f3f9ddbe380c4781e6341dd9cc1a71153d3d8e5e
Crystal B <crystalbohacek@gmail.com>
2018-10-24 18:20:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ function viewCart() {
   } else if (cart.length === 2) {
     return `In your cart, you have ${itemArray[0]} at $${priceArray[0]}, and ${itemArray[1]} at $${priceArray[1]}`
   } else {
-    str =  `In your cart, you have ${itemArray[0]} at $${priceArray[0]}`
-    for (i=1; i<=cart.length-2; i++){
+    var str =  `In your cart, you have ${itemArray[0]} at $${priceArray[0]}`
+    for (var i=1; i<=cart.length-2; i++){
       str+= `, ${itemArray[i]} at $${priceArray[i]}`
     }
     str+= `, and ${itemArray[itemArray.length-1]} at $${itemArray.length-1}.`
